c258ea4cb90e787d42af02a5d25e431bf6792c65
yangshengqing <340711340@qq.com>
2016-03-01 13:43:12 +0800

    完成第二个界面的布局

---
+++ src/com/ruili/target/activity/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.ruili.target.activity;
+
+import com.ruili.target.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LoginActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Button btnLogin = (Button) findViewById(R.id.btn_login);
+		btnLogin.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.btn_login) {
+			startActivity(new Intent(this, MainActivity.class));
+			finish();
+		}
+	}
+
+}
